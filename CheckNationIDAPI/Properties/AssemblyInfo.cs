6d1151ce051a4bcebea7e4e2fc7cccb8a108d7c5
adison_ken <adison.ken@gmail.com>
2019-10-03 11:23:07 +0700

    init Project

---
+++ CheckNationIDAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CheckNationIDAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CheckNationIDAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d58cf7d6-d965-47b1-b9cf-23e726e1156b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CheckNationIDAPI/Service1.svc.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CheckNationIDAPI
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public bool IsValidCheckPersonID(string pid)
+        {
+            char[] numberChars = pid.ToCharArray();
+
+            int total = 0;
+            int mul = 13;
+            int mod = 0, mod2 = 0;
+            int nsub = 0;
+            int numberChars12 = 0;
+
+            for (int i = 0; i < numberChars.Length - 1; i++)
+            {
+                int num = 0;
+                int.TryParse(numberChars[i].ToString(), out num);
+
+                total = total + num * mul;
+                mul = mul - 1;
+
+                //Debug.Log(total + " - " + num + " - "+mul);
+            }
+
+            mod = total % 11;
+            nsub = 11 - mod;
+            mod2 = nsub % 10;
+
+            //Debug.Log(mod);
+            //Debug.Log(nsub);
+            //Debug.Log(mod2);
+
+
+            int.TryParse(numberChars[12].ToString(), out numberChars12);
+
+            //Debug.Log(numberChars12);
+
+            if (mod2 != numberChars12)
+                return false;
+            else
+                return true;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
